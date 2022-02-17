de680f4e0f9d9ef93ba57488dee7b81f95b5136c
PauloCesar410 <linhares@ufersa.edu.br>
2022-02-17 00:02:58 -0300

    Programando em GO:variaveis e pacotes

---
+++ Variaveis/vaiariaveis.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "Variável 1"
+	variavel2 := "Variável2"
+
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+
+	var (
+		variavel3 string = "lalallal"
+		variavel4 string = "fefefefe"
+	)
+
+	fmt.Println(variavel3, variavel4)
+
+	variavel5, variavel6 := "Variável 5", "Variável6"
+
+	fmt.Println(variavel5, variavel6)
+
+	const constante1 string = "Constante 1"
+	fmt.Println(constante1)
+
+	variavel5, variavel6 = variavel6, variavel5
+	fmt.Println(variavel5, variavel6)
+
+}
